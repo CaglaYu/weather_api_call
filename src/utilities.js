61501c04c48636c47bac12f7d6fa3876d3531cef
CaglaYu <caglasi@hotmail.com>
2022-02-04 23:27:44 +0100

    weather

---
+++ src/utilities.js
@@ -0,0 +1,13 @@
+const range = (start, end, step = 1) => {
+    let output = [];
+    if (typeof end === 'undefined') {
+      end = start;
+      start = 0;
+    }
+    for (let i = start; i < end; i += step) {
+      output.push(i);
+    }
+    return output;
+  };
+  const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  export {random,range}
